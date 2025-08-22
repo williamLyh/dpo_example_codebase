0af118854a2bc8b4d631e40543f76b7ae2052c3c
Yinhong Liu <yinhongliu@Yinhongs-MacBook-Pro.local>
2025-08-22 16:05:43 +0100

    init

--- playground.py
+++ playground.py
@@ -65,7 +65,6 @@ def apply_chat_template(prompt, response, tokenizer):
 def collate_fn(batch):
     prompt_chosen_texts = []
     prompt_rejected_texts = []
-    prompt_only_texts = []
     prompt_offset_lens = []
     for item in batch:
         prompt_chosen_texts.append(apply_chat_template(item['prompt'], item['chosen'], tokenizer))
@@ -90,7 +89,12 @@ loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=col
 
 
 def get_sequence_logprobs(ids, logits, attention_mask, prompt_offset_lens):
-    logprobs = F.log_softmax(logits[:, :-1, :].contiguous(), dim=-1)
+    '''ids shape: (B, L)
+       logits shape: (B, L, V)
+       attention_mask shape: (B, L)
+       prompt_offset_lens shape: (B,)'''
+    
+    logprobs = F.log_softmax(logits[:, :-1, :].contiguous(), dim=-1)  # logits and index offset by 1 
     logprobs_selected = torch.gather(logprobs, dim=-1, index=ids[:, 1:].unsqueeze(-1)).squeeze(-1)
     logprobs_selected = logprobs_selected * attention_mask[:, :-1]
     seq_logprobs = []
@@ -101,8 +105,12 @@ def get_sequence_logprobs(ids, logits, attention_mask, prompt_offset_lens):
     return seq_logprobs
 
 
+optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
 
-for batch in loader:
+
+for batch in tqdm(loader):
+    optimizer.zero_grad()
+
     prompt_chosen_batch = batch['prompt_chosen_ids'].to(DEVICE)
     prompt_chosen_batch_attention_mask = batch['prompt_chosen_attention_mask'].to(DEVICE)
     prompt_rejected_batch = batch['prompt_rejected_ids'].to(DEVICE)
@@ -128,76 +136,7 @@ for batch in loader:
     log_ratio_chosen = chosen_seq_logprob - chosen_ref_seq_logprob
     log_ratio_rejected = rejected_seq_logprob - rejected_ref_seq_logprob
     loss = -torch.log(torch.sigmoid((log_ratio_chosen - log_ratio_rejected)/BETA)).mean()
+
     print(loss.item())
-
-    assert False
-
-
-# -----------------------
-# 优化器
-# -----------------------
-optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
-
-
-
-# -----------------------
-# 训练循环
-# -----------------------
-for epoch in range(EPOCHS):
-    for batch in tqdm(loader):
-        optimizer.zero_grad()
-        losses = []
-
-        prompt_ids = batch['prompt_ids'].to(DEVICE)
-        chosen_ids = batch['chosen_ids'].to(DEVICE)
-        rejected_ids = batch['rejected_ids'].to(DEVICE)
-
-        chosen_lo
-        # print('-------------------')
-        # print(prompt_ids.shape, chosen_ids.shape, rejected_ids.shape)
-        # assert False
-
-        # prompt_ids = torch.tensor([item['prompt_ids'] for item in batch]).to(DEVICE)
-        # chosen_ids = torch.tensor([item['chosen_ids'] for item in batch]).to(DEVICE)
-        # rejected_ids = torch.tensor([item['rejected_ids'] for item in batch]).to(DEVICE)
-
-
-        for item in batch:
-            prompt_ids = torch.tensor(item["prompt_ids"]).to(DEVICE)
-            chosen_ids = torch.tensor(item["chosen_ids"]).to(DEVICE)
-            rejected_ids = torch.tensor(item["rejected_ids"]).to(DEVICE)
-
-            print('--------------------------------')
-            print(prompt_ids.shape, chosen_ids.shape, rejected_ids.shape)
-            # 模型 log probs
-            chosen_logits = model(input_ids=prompt_ids.unsqueeze(0), labels=chosen_ids.unsqueeze(0)).logits
-            rejected_logits = model(input_ids=prompt_ids.unsqueeze(0), labels=rejected_ids.unsqueeze(0)).logits
-
-            # reference log probs
-            with torch.no_grad():
-                chosen_logits_ref = ref_model(input_ids=prompt_ids.unsqueeze(0), labels=chosen_ids.unsqueeze(0)).logits
-                rejected_logits_ref = ref_model(input_ids=prompt_ids.unsqueeze(0), labels=rejected_ids.unsqueeze(0)).logits
-
-            # log_softmax
-            chosen_log_probs = F.log_softmax(chosen_logits, dim=-1)
-            rejected_log_probs = F.log_softmax(rejected_logits, dim=-1)
-            chosen_log_probs_ref = F.log_softmax(chosen_logits_ref, dim=-1)
-            rejected_log_probs_ref = F.log_softmax(rejected_logits_ref, dim=-1)
-
-            # 聚合序列 log prob
-            chosen_seq_log_prob = chosen_log_probs.gather(2, chosen_ids.unsqueeze(0).unsqueeze(-1)).squeeze(-1).sum()
-            rejected_seq_log_prob = rejected_log_probs.gather(2, rejected_ids.unsqueeze(0).unsqueeze(-1)).squeeze(-1).sum()
-            chosen_seq_log_prob_ref = chosen_log_probs_ref.gather(2, chosen_ids.unsqueeze(0).unsqueeze(-1)).squeeze(-1).sum()
-            rejected_seq_log_prob_ref = rejected_log_probs_ref.gather(2, rejected_ids.unsqueeze(0).unsqueeze(-1)).squeeze(-1).sum()
-
-            # DPO loss
-            log_ratio_chosen = chosen_seq_log_prob - chosen_seq_log_prob_ref
-            log_ratio_rejected = rejected_seq_log_prob - rejected_seq_log_prob_ref
-            loss = -torch.log(torch.sigmoid((log_ratio_chosen - log_ratio_rejected)/BETA))
-            losses.append(loss)
-
-        batch_loss = torch.stack(losses).mean()
-        batch_loss.backward()
-        optimizer.step()
-
-    print(f"Epoch {epoch+1} Loss: {batch_loss.item():.4f}")
+    loss.backward()
+    optimizer.step()
